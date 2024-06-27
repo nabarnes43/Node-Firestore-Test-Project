751041cc842734fa511217dc64aae702caceb369
nabarnes43 <89264306+nabarnes43@users.noreply.github.com>
2024-06-26 20:02:03 -0400

    Update config.js remove info

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ const firebase = require('firebase');
 require('firebase/firestore');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9Ks-IYtUU29mcZJo_Tht2WdjSf_K7CQU",
-  authDomain: "api-test-427120.firebaseapp.com",
-  projectId: "gmail-api-test-427120",
-  storageBucket: "gmail-api-test-427120.appspot.com",
-  messagingSenderId: "351616001587",
-  appId: "1:351616001587:web:c407a7390f6ad22eb36546",
-  measurementId: "G-6LF9ZNNJNZ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
